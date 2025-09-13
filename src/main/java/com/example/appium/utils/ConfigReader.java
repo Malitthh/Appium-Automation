ed5f62325c1796e05a7e4d999d5f4301797c69b6
Malith Senadheera <malith.akalanka@outlook.com>
2025-09-13 19:58:17 +0530

    added new files

--- src/main/java/com/example/appium/utils/ConfigReader.java
+++ src/main/java/com/example/appium/utils/ConfigReader.java
@@ -1,5 +1,20 @@
 package com.example.appium.utils;
 
-public class ConfigReader {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
+public class ConfigReader {
+    private static Properties prop;
+
+    public static void loadProperties() throws IOException {
+        prop = new Properties();
+        FileInputStream fis = new FileInputStream("src/resources/config.properties");
+        prop.load(fis);
+    }
+
+    public static String get(String key) {
+        return prop.getProperty(key);
+    }
 }
+
